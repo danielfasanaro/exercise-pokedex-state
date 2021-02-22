9a02d1e3c6acdf5280a840a80bfe60423c1a9d6f
danielfasanaro <danielfasanaro@gmail.com>
2021-02-22 19:24:47 -0300

    Modify Pokedex to render only 1 pokemon at a time and add nextPokemon button

--- src/Pokedex.js
+++ src/Pokedex.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import Pokemon from './Pokemon';
+import './Pokedex.css'
 
 class Pokedex extends React.Component {
+    constructor(props) {
+      super(props)
+      this.state = { visible: 0 };
+    }
+
+    nextPokemon = () => {
+      if (this.state.visible < this.props.pokemons.length - 1) this.setState( { visible: this.state.visible + 1 } );
+      else this.setState({ visible: 0 });
+    }
+
     render() {
         return (
             <div className="pokedex">
-                {this.props.pokemons.map(pokemon => <Pokemon key={pokemon.id} pokemon={pokemon} />)}
+              <Pokemon key={ this.props.pokemons[this.state.visible] } pokemon={ this.props.pokemons[this.state.visible] } />
+              <button className="nextPokemonBtn" onClick={ this.nextPokemon }>Next Pokemon</button>
             </div>
         );
     }
